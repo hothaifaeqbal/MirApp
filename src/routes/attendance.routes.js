143fbc44ac164e7629691942bbd1aa615400ebae
Hothaifa Eqbal <darauemaror@gmail.com>
2026-03-27 20:14:41 +0100

    Update attendance.routes.js

--- src/routes/attendance.routes.js
+++ src/routes/attendance.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { checkIn, checkOut } from '../controllers/attendance.controller.js';
-import { protect } from '../middlewares/auth.middleware.js';
+import { checkIn, checkOut, kioskScan } from '../controllers/attendance.controller.js';
+import { protect, adminOnly } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/check-in', protect, checkIn);
 router.post('/check-out', protect, checkOut);
+router.post('/kiosk-scan', protect, adminOnly, kioskScan);
 
 export default router;
